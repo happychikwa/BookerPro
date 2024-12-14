261a8075d260cc3a9a286d4e35174d6cb99628c4
happychikwa <erastuschikwa@gmail.com>
2024-12-14 17:55:11 +0200

    customer type and placeholder data

---
+++ bookerpro/app/lib/definitions.ts
@@ -0,0 +1,7 @@
+export type Invoice = {
+    customer_id: number,
+    total: number,
+    apartment_name: [string,number],
+    checkin_date: string,
+    checkout_date: string
+}
---
+++ bookerpro/app/lib/placeholder-data.ts
@@ -0,0 +1,17 @@
+const active_reservation = [
+    {
+        customer_id: 1,
+        total: 650.00,
+        apartment_name: ["fully furnished",650],
+        checkin_date: "14/12/2024",
+        checkout_date: "15/12/2024"
+    },
+    {
+        customer_id: 2,
+        total: 1350.00,
+        apartment_name: ["Single room",450],
+        checkin_date: "15/12/2024",
+        checkout_date: "18/12/2024"
+    }
+    
+]
